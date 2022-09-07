e4315ea9c0b8ec243c4f8c37b976b321a9550fee
drkchiloll <samwomack_@me.com>
2022-09-07 11:25:44 -0500

    optimize GetContainerView .. write Methods to Post Configs to ESX (AddPortGroup/Update Virtual Switch)

--- apgjb.go
+++ apgjb.go
@@ -6,12 +6,14 @@ import (
 
 	"github.com/vmware/govmomi/object"
 	"github.com/vmware/govmomi/view"
+	"github.com/vmware/govmomi/vim25/methods"
 	"github.com/vmware/govmomi/vim25/mo"
+	"github.com/vmware/govmomi/vim25/types"
 )
 
 // Jumpbox ...
 type Jumpbox struct {
-	esxiClient *esxClient
+	EsxiClient *esxClient
 	ctx        context.Context
 }
 
@@ -19,30 +21,54 @@ type Jumpbox struct {
 func NewJumpbox(host, user, pass string) *Jumpbox {
 	ctx := context.Background()
 	uri := fmt.Sprintf("https://%s/sdk", host)
-	return &Jumpbox{
-		esxiClient: newEsxClient(ctx, uri, user, pass),
-		ctx:        ctx,
+	client := newEsxClient(ctx, uri, user, pass)
+
+	return &Jumpbox{ctx: ctx, EsxiClient: client}
+}
+
+func (j *Jumpbox) Login() error {
+	return j.EsxiClient.Login(j.ctx, j.EsxiClient.Userinfo)
+}
+
+func (j *Jumpbox) Logout() error {
+	return j.EsxiClient.Logout(j.ctx)
+}
+
+func (j *Jumpbox) getView(v string) (*view.ContainerView, error) {
+	m := view.NewManager(j.EsxiClient.Client)
+	return m.CreateContainerView(
+		j.ctx,
+		j.EsxiClient.ServiceContent.RootFolder,
+		[]string{v},
+		true,
+	)
+}
+
+func (j *Jumpbox) GetHosts() ([]mo.HostSystem, error) {
+	v, err := j.getView("HostSystem")
+	if err != nil {
+		return nil, err
 	}
+	defer v.Destroy(j.ctx)
+	var hosts []mo.HostSystem
+	err = v.Retrieve(j.ctx, []string{"HostSystem"}, nil, &hosts)
+	if err != nil {
+		return nil, err
+	}
+	return hosts, nil
 }
 
 // GetVmsWithTickets ...
 func (j *Jumpbox) GetVmsWithTickets() []ApgVM {
-	defer j.esxiClient.Logout(j.ctx)
-	vman := view.NewManager(j.esxiClient.Client)
-
-	cview, _ := vman.CreateContainerView(
-		j.ctx,
-		j.esxiClient.ServiceContent.RootFolder,
-		[]string{"VirtualMachine"},
-		true,
-	)
+	v, _ := j.getView("VirtualMachine")
+	defer v.Destroy(j.ctx)
 	var vmMos []mo.VirtualMachine
-	cview.Retrieve(j.ctx, []string{"VirtualMachine"}, []string{"summary"}, &vmMos)
+	v.Retrieve(j.ctx, []string{"VirtualMachine"}, []string{"summary"}, &vmMos)
 
 	var vms []ApgVM
 
 	for _, vmMo := range vmMos {
-		vm := object.NewVirtualMachine(j.esxiClient.Client, vmMo.Reference())
+		vm := object.NewVirtualMachine(j.EsxiClient.Client, vmMo.Reference())
 		vmTicket, _ := vm.AcquireTicket(j.ctx, "mks")
 		apgVM := ApgVM{
 			UUID:         vmMo.Summary.Config.Uuid,
@@ -58,4 +84,87 @@ func (j *Jumpbox) GetVmsWithTickets() []ApgVM {
 		vms = append(vms, apgVM)
 	}
 	return vms
+}
+
+func (j *Jumpbox) GetNetworks() {
+	v, _ := j.getView("Network")
+	defer v.Destroy(j.ctx)
+	var networks []mo.Network
+	v.Retrieve(j.ctx, []string{"Network"}, nil, &networks)
+	for _, n := range networks {
+		fmt.Println(n.Name)
+	}
+}
+
+type AddPgParams struct {
+	// A Reference to the HostNetworkSystem
+	// host.ConfigManager.NetworkSystem.Reference()
+	HostNetSystemRef types.ManagedObjectReference
+	PgName           string
+	PgVlanId         int
+	VswitchName      string
+	Security         NetSec
+}
+
+type NetSec struct {
+	AllowPromiscuous bool
+	AllowMacChange   bool
+	ForgedXmits      bool
+}
+
+// AddPG adds a PortGroup to an Existing vSwitch
+func (j *Jumpbox) AddPG(p AddPgParams) error {
+	policy := types.HostNetworkPolicy{}
+	if p.Security.AllowPromiscuous {
+		*policy.Security.AllowPromiscuous = p.Security.AllowPromiscuous
+	}
+	if p.Security.AllowMacChange {
+		*policy.Security.MacChanges = p.Security.AllowMacChange
+	}
+	if p.Security.ForgedXmits {
+		*policy.Security.ForgedTransmits = p.Security.ForgedXmits
+	}
+	_, err := methods.AddPortGroup(j.ctx, j.EsxiClient.Client, &types.AddPortGroup{
+		This: p.HostNetSystemRef,
+		Portgrp: types.HostPortGroupSpec{
+			Name:        p.PgName,
+			VlanId:      int32(p.PgVlanId),
+			VswitchName: p.VswitchName,
+			Policy:      policy,
+		},
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type VswitchOp struct {
+	Name     string
+	ChangeOp types.HostConfigChangeOperation
+	Specs    *types.HostVirtualSwitchSpec
+}
+
+type VswitchPostParams struct {
+	HostNetSystemRef types.ManagedObjectReference
+	Vswitch          VswitchOp
+	ChangMode        types.HostConfigChangeMode
+}
+
+func (j *Jumpbox) VswitchPost(p VswitchPostParams) error {
+	_, err := methods.UpdateNetworkConfig(j.ctx, j.EsxiClient.Client, &types.UpdateNetworkConfig{
+		This: p.HostNetSystemRef,
+		Config: types.HostNetworkConfig{
+			Vswitch: []types.HostVirtualSwitchConfig{{
+				ChangeOperation: string(p.Vswitch.ChangeOp),
+				Name:            p.Vswitch.Name,
+				Spec:            p.Vswitch.Specs,
+			}},
+		},
+		ChangeMode: string(p.ChangMode),
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
