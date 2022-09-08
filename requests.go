21ac239398f9f770feebe9a52616b47e6a908898
drkchiloll <samwomack_@me.com>
2022-09-08 09:26:52 -0500

    http requests for Esxi File Uploads

---
+++ requests.go
@@ -0,0 +1,37 @@
+package apgjb
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+type Service struct {
+	http    *http.Client
+	BaseURL string
+}
+
+func newHttpService(host string, jar *http.CookieJar) *Service {
+	return &Service{
+		BaseURL: fmt.Sprintf("https://%s/folder", host),
+		http: &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{
+					InsecureSkipVerify: true,
+				},
+			},
+			Timeout: 1200 * time.Second,
+			Jar:     *jar,
+		},
+	}
+}
+
+func (s *Service) GenerateRequest(method, url string, file *os.File) (*http.Request, error) {
+	return http.NewRequest(method, url, file)
+}
+
+func (s *Service) MakeRequest(req *http.Request) (*http.Response, error) {
+	return s.http.Do(req)
+}
